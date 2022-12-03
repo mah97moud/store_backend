4194f225fcdb9d444ebc7c2df501e8e7160bb77a
Mahmoud Atef <mahmoudatif54@yahoo.com>
2022-12-04 00:54:37 +0200

    products test endpoint

--- spec/tests/endpoints/productsSpec.ts
+++ spec/tests/endpoints/productsSpec.ts
@@ -1,3 +1,4 @@
+import { Product } from './../../../src/models/product'
 import supertest from 'supertest'
 import app from '../../../src/server'
 
@@ -13,6 +14,28 @@ describe('Test products Endpoint responses', () => {
   it('Successful GET[/products]', async () => {
     const response = await request
       .get('/products')
+      .set('Authorization', `Bearar ${token}`)
+    // productId = response.body.id
+    expect(response.status).toBe(200)
+  })
+
+  it('Successful Show Product GET[/product/:id]', async () => {
+    const response = await request
+      .get('/product/1')
+      .set('Authorization', `Bearar ${token}`)
+    expect(response.status).toBe(200)
+  })
+
+  it('Successful create Product GET[/product/create]', async () => {
+    const product: Product = {
+      productName: 'Test',
+      price: 20,
+      category: 'test',
+    }
+
+    const response = await request
+      .post('/product/create')
+      .send(product)
       .set('Authorization', `Bearar ${token}`)
     expect(response.status).toBe(200)
   })
